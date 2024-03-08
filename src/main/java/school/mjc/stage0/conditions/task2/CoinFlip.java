5338c32216cbd5439a631b5bea1b7c95ceea6a8f
murod <murad.djanikulov@gmail.com>
2024-03-09 00:01:20 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        boolean x = from1UpTo1000 <500;
+        if (x){
+            System.out.println ("Eagle");}
+        else {
+            System.out.println("Tail");}
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        boolean x = first>=second;
+        if ( x ){
+            System.out.println(first);}
+        else {
+            System.out.println(second);}
     }
 }
